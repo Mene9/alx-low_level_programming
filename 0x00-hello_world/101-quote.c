6ef39a39a11d22af9cb62a1bf311e97ce69bd3f2
Mene <harrility@gmail.com>
2022-09-11 10:55:43 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (success)
+ */
+/* Discription: To return 1*/
+int main(void)
+{
+write(2, "and that piece of art is useful\"- Dora korpar, 2015-10-19\n");
+return (1);
+}
