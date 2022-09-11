7ee66ec4651d330a15c5fd3b1e18d7cd48576826
Mene <harrility@gmail.com>
2022-09-11 11:07:07 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * Discription: "return 1"
  * Return: Always 1 (success)
  */
-/* Discription: To return 1*/
 int main(void)
 {
-write(2, "and that piece of art is useful\"- Dora korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\"- Dora korpar,2015-10-19\n");
 return (1);
 }
