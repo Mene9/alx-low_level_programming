c71dbdff0498b320802969be26fff1f4dd867b4c
Mene <harrility@gmail.com>
2022-09-11 09:00:32 -0700

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 printf("Size of a char: %ld 1 byte(s)\n", sizeof(char));
-printf("size of an int: %ld 4 byte(s)\n", sizeof(int));
-printf("size of a long int: %ld 4 byte(s)\n", sizeof(long int));
-printf("size of a long long int: %ld 8 byte(s)\n", sizeof(long long int));
-printf("size of a float: %ld 4 byte(s)\n", sizeof(float));
+printf("Size of an int: %ld 4 byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld 4 byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld 8 byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld 4 byte(s)\n", sizeof(float));
 return (0);
 }
