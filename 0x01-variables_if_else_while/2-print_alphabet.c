02a34d13ddd659bb1bda690cd4b470cc9f3892ec
Mene <harrility@gmail.com>
2022-09-12 08:44:51 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdlib.h>
 /**
+ *
  *main - Entry point
  *Description:Write a program that prints the alphabet in lowercase
  * return: always 0 (success)
- *
  */
 int main(void)
 {
