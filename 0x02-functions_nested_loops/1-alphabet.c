bf902594fb77cde9035004c2479985e7f2ed4bbb
Mene <harrility@gmail.com>
2022-09-13 21:36:47 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char n;
+
+for (n = 'a'; n <= 'z';)
+_putchar(n)
+
+_putchar("\n");
+
+return (0);
+}
