a856ea5b791fd6520991c57a0f0597e48213041b
Mene <harrility@gmail.com>
2022-09-14 17:09:05 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	char *sh = "main";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
