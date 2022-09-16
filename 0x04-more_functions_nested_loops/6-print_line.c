253e2fd8ecbc657034f4da1be00dbc46e51904c7
Mene <harrility@gmail.com>
2022-09-16 15:08:40 -0700

    Task 7

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,20 @@
-nclude "main.h"
+#include "main.h"
 
 /**
+ *
  * print_line - draws a straight line in the terminal
- * @n: the number of times the character `_` would be printed*
+ * @n: the number of times the character `_` would be printed
  *
  * Return: void
  */
-
 void print_line(int n)
 {
 	for (; n > 0; n--)
+	
 	{
 		_putchar('_');
 	}
+	
 	_putchar('\n');
 
 }
