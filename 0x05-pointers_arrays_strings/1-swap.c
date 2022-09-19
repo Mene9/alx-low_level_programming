e3890d898538b64d51f3fe8aae6b303ac47be5b6
Mene <harrility@gmail.com>
2022-09-19 10:59:11 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - swap the values of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
