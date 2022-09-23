f26f37031c23d0ac0b453eb241137a1cecdeb88a
Mene <harrility@gmail.com>
2022-09-23 16:37:02 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
- * Discription: "return 1"
- * Return: Always 1 (success)
+ * main - print quote
+ *
+ * Description: prints Dors's quote
+ *
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
